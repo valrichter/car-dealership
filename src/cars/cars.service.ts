a5bb24448e55276de46e15011a282b408ac929f1
valrichter <valenrichter11@gmail.com>
2023-04-01 21:02:40 -0300

    add cars

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Supra',
+    },
+    {
+      id: 2,
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: 3,
+      brand: 'Nissan',
+      model: 'GT',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+    return car;
+  }
+}
