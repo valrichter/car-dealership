c0ee71b5ef89609e9b86bd1035ed5f2a9cca0e53
valrichter <valenrichter11@gmail.com>
2023-04-01 21:23:36 -0300

    add car exeptions

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -10,7 +10,8 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById(@Param('id') id: string) {
-    return this.carsService.findOneById(+id);
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    console.log({ id });
+    return this.carsService.findOneById(id);
   }
 }
