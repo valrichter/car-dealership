ca5c536add9d22c25f757e5a9c4b7df1f8be2d6d
valrichter <valenrichter11@gmail.com>
2023-04-01 21:38:27 -0300

    add CRUD completely

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -13,5 +23,23 @@ export class CarsController {
   getCarById(@Param('id', ParseIntPipe) id: number) {
     console.log({ id });
     return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar(@Body() body: any) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar(@Body() body: any) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'DELETE',
+      id,
+    };
   }
 }
