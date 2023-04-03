580e8b81179d4a36bf501ddc9f84f53c74a926d1
valrichter <valenrichter11@gmail.com>
2023-04-03 18:51:21 -0300

    add Validation Pipes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -8,11 +8,14 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
+@UsePipes(ValidationPipe)
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
   @Get()
