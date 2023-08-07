4d559ccd5c3ccd6ab40299204f4a87e9ea16e07e
valrichter <valenrichter11@gmail.com>
2023-08-07 01:39:22 +0000

    add BRANDS_SEED

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CarsService } from '../cars/cars.service';
 import { CARS_SEED } from './data/cars.seed';
+import { BRANDS_SEED } from './data/brands.seed';
+import { BrandsService } from 'src/brands/brands.service';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly carsService: CarsService) {}
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
 
   populateDb() {
     this.carsService.fillCarsWithSeedData(CARS_SEED);
-
+    this.brandsService.fillBrandsWithSeedData(BRANDS_SEED);
     return 'SEED EXECUTED';
   }
 }
